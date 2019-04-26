a23767d457adeb751e290a399445336493d2558c
Patrick Kim <pkim050@gmail.com>
2019-04-26 11:06:19 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,13 @@
 class AuthorsController < ApplicationController
-  before_action :set_author!, only: [:new, :create, :show]
-
   def show
+    @author = Author.find(params[:id])
   end
 
   def new
   end
 
   def create
-    @author = Author.create!(author_params)
+    @author = Author.new(author_params)
     if @author.save
       redirect_to @author
     else
@@ -20,9 +19,5 @@ class AuthorsController < ApplicationController
 
   def author_params
     params.permit(:email, :name)
-  end
-
-  def set_author!
-    @author = Author.find(params[:id])
   end
 end
